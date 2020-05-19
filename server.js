bc68d34540ff5445f4d23607491f694f8e934de9
zaryanz <aryanx404@gmail.com>
2020-05-19 16:53:14 +0530

    Client boilerplate, changed port to 5000, conflicting with client

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const colors = require('colors');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const colors = require("colors");
+const jwt = require("jsonwebtoken");
 
-const multer = require('multer');
-const upload = multer({dest:'uploads/'});
-const connectDB = require('./config/db.js');
-const Profile = require('./models/Profile.js');
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const connectDB = require("./config/db.js");
+const Profile = require("./models/Profile.js");
 // PLC at home using Arduino and CODESYS-Automation Control lab
 // Connect to DB
 connectDB();
@@ -16,11 +16,13 @@ const app = express();
 app.use(express.json());
 
 // @ROUTES
-app.use('/todo', require('./routes/todos.js'));
-app.use('/sort', require('./routes/sort.js'));
-app.use('/tag', require('./routes/tags.js'));
-app.use('/login', require('./routes/auth.js'));
+app.use("/todo", require("./routes/todos.js"));
+app.use("/sort", require("./routes/sort.js"));
+app.use("/tag", require("./routes/tags.js"));
+app.use("/login", require("./routes/auth.js"));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT , () => { console.log(`Console running on port ${PORT}`) });
+app.listen(PORT, () => {
+  console.log(`Console running on port ${PORT}`);
+});
