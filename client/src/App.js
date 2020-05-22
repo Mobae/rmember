205d9b6d7dd161fce36d36455a64803fa2224b95
zaryanz <aryanx404@gmail.com>
2020-05-22 09:19:11 +0530

    Basic router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
-import Navigation from './Components/Navigation'
-import IntroSection from './Components/IntroSection'
-// import { BrowserRouter, Route } from 'react-router-dom'
+import Navigation from "./Components/Navigation";
+import IntroSection from "./Components/IntroSection";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
 
 const App = () => {
   return (
     <div>
+      <Router>
         <Navigation />
-        
-        <IntroSection />
+        <Route location="/" exact component={IntroSection} />
+      </Router>
     </div>
   );
-  
 };
 
 export default App;
--- client/src/Components/Navigation.js
+++ client/src/Components/Navigation.js
@@ -1,22 +1,35 @@
-import React from 'react';
-import { Navbar, Nav, Form, NavLink, FormControl, Button } from 'react-bootstrap';
-// import { Link, NavLink } from 'react-router-dom';
+import React from "react";
+import {
+  Navbar,
+  Nav,
+  Form,
+  NavLink,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <Navbar bg="dark" variant="dark" style={{ position: "sticky", top: 0 }}>
-    <Navbar.Brand href="/">Navbar</Navbar.Brand>
-    <Nav className="mr-auto">
-      <NavLink to="/">Item1</NavLink>
-      <NavLink to="/Item2">Item2</NavLink>
-      <NavLink to="/Item3">Item3</NavLink>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search Something" className="mr-sm-2" />
-      <Button variant="outline-secondary">Search</Button>
-    </Form>
+      <Link to="/">
+        <Navbar.Brand>Navbar</Navbar.Brand>
+      </Link>
+      <Nav className="mr-auto">
+        <NavLink to="/">Item1</NavLink>
+        <NavLink to="/Item2">Item2</NavLink>
+        <NavLink to="/Item3">Item3</NavLink>
+      </Nav>
+      <Form inline>
+        <FormControl
+          type="text"
+          placeholder="Search Something"
+          className="mr-sm-2"
+        />
+        <Button variant="outline-secondary">Search</Button>
+      </Form>
     </Navbar>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
