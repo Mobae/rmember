f4b5c96d24e575dfb95076bcf96213308f2e2e7a
zaryanz <aryanx404@gmail.com>
2020-05-26 18:30:12 +0530

    intro section updated

--- client/src/Components/Pages/IntroSection.js
+++ client/src/Components/Pages/IntroSection.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import Batman from "../batman.png";
+import intro from "../intro.jpg";
 import { Link } from "react-router-dom";
 
 const IntroSection = () => {
@@ -10,9 +10,8 @@ const IntroSection = () => {
         <div className="content">
           <div style={{ display: "flex", flexDirection: "row" }}>
             <div>
-              <h1>
-                Here We Can Write Main Slogan or Something for our Application.{" "}
-              </h1>
+              <h1>Rmember</h1>
+              <h3>Task Management Simplified</h3>
               <p>
                 Then some Deatils for our app. Rmember's boards, lists, and
                 cards enable you to organize and prioritize your projects in a
@@ -25,73 +24,17 @@ const IntroSection = () => {
             </div>
             <div>
               <img
-                src={Batman}
+                src={intro}
                 alt=""
-                style={{ height: "10rem", width: "10rem" }}
+                style={{
+                  display: "block",
+                  maxWidth: "500px",
+                  width: "auto",
+                  height: "auto",
+                }}
               />
             </div>
           </div>
-          <Button id="sign-up-button" variant="success">
-            <Link to="/signup">Sign Up For Free</Link>
-          </Button>
-        </div>
-      </div>
-      <br />
-      {/* Will insert an image after every p tag  */}
-      <div className="info1">
-        <div className="content">
-          <h2>Never Forget with Rmember</h2>
-          <p>
-            You Forgot Some Shit today?? Rmember is there for you. Rmember is
-            your own personalized todo which makes your life hastle free. Lorem
-            ipsum dolor sit amet consectetur adipisicing elit. Qui praesentium
-            natus totam rerum labore, officiis veniam exercitationem iste
-            adipisci repellat tempore vel quo esse, ad cum, aliquam ea. Quidem,
-            temporibus?
-          </p>
-          <div>
-            <img
-              src={Batman}
-              alt=""
-              style={{ height: "10rem", width: "10rem" }}
-            />
-          </div>
-          <Button id="sign-up-button" variant="success">
-            <Link to="/signup">Never Forget Again</Link>
-          </Button>
-        </div>
-      </div>
-      <br />
-      <div className="info2">
-        <div className="content">
-          <h2>Never Forget with Rmember</h2>
-          <p>
-            You Forgot Some Shit today?? Rmember is there for you. Rmember is
-            your own personalized todo which makes your life hastle free. Lorem
-            ipsum dolor sit amet consectetur adipisicing elit. Id iste dolore
-            quisquam, quam ab et, voluptas error quae eaque quasi cum mollitia
-            ratione autem tenetur sint voluptate odit officia optio.
-          </p>
-          <Button id="sign-up-button" variant="success">
-            <Link to="/signup">Never Forget Again</Link>
-          </Button>
-        </div>
-      </div>
-      <br />
-      <div className="info3">
-        <div className="content">
-          <h2>Never Forget with Rmember</h2>
-          <p>
-            You Forgot Some Shit today?? Rmember is there for you. Rmember is
-            your own personalized todo which makes your life hastle free. Lorem
-            ipsum dolor sit amet consectetur adipisicing elit. Sit quasi
-            delectus quia alias, cum beatae, autem assumenda et, praesentium
-            nulla mollitia natus voluptatem dicta id amet earum impedit!
-            Voluptatum, nostrum?
-          </p>
-          <Button id="sign-up-button" variant="success">
-            <Link to="/signup">Never Forget Again</Link>
-          </Button>
         </div>
       </div>
     </div>
