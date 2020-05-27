2ce4d34ef47e55478702ca15dca451f09946f6a4
zaryanz <aryanx404@gmail.com>
2020-05-27 10:22:59 +0530

    Intro section changes

--- client/src/Components/Pages/IntroSection.js
+++ client/src/Components/Pages/IntroSection.js
@@ -4,23 +4,42 @@ import intro from "../intro.jpg";
 import { Link } from "react-router-dom";
 
 const IntroSection = () => {
+  const styles = {
+    bulletPts: {
+      marginTop: "1em",
+      fontSize: "16.5px",
+    },
+    bulletContainer: {
+      marginTop: "2em",
+      marginBottom: "2em",
+    },
+  };
   return (
     <div>
       <div className="main-intro">
         <div className="content">
-          <div style={{ display: "flex", flexDirection: "row" }}>
+          <div
+            style={{ display: "flex", flexDirection: "row", marginTop: "2em" }}
+          >
             <div>
               <h1>Rmember</h1>
-              <h4>Task Management Simplified</h4>
-              <p>
-                Then some Deatils for our app. Rmember's boards, lists, and
-                cards enable you to organize and prioritize your projects in a
-                fun, flexible, and rewarding way. . Lorem ipsum dolor sit amet,
-                consectetur adipisicing elit. Voluptates dolorem perferendis
-                quaerat alias facere nesciunt laudantium vero omnis tempora,
-                itaque veritatis temporibus iure nam. Reprehenderit est eius
-                fuga totam id!
-              </p>
+              <h3>Task Management Simplified</h3>
+              <div style={styles.bulletContainer}>
+                <p style={styles.bulletPts}>
+                  <i className="chevron circle right icon large"></i>
+                  Effortless task scheduling done with Rmember, so you can focus
+                  on what matters the most
+                </p>
+                <p style={styles.bulletPts}>
+                  <i className="chevron circle right icon large"></i>
+                  Get notifications on your favorite platforms like WhatsApp,
+                  Telegram and Discord
+                </p>
+                <p style={styles.bulletPts}>
+                  <i className="chevron circle right icon large"></i>
+                  Free of cost forever
+                </p>
+              </div>
               <button className="ui animated button blue" tabIndex="0">
                 <Link to="/signup">
                   <div className="visible content">Get Started</div>
@@ -37,6 +56,8 @@ const IntroSection = () => {
                   maxWidth: "500px",
                   width: "auto",
                   height: "auto",
+                  marginLeft: "2em",
+                  borderRadius: "2px",
                 }}
               />
             </div>
