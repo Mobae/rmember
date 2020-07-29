86558f632df3fa6659241367ffa215686f666339
sageOMEGA <abhishekdhakad61@gmail.com>
2020-07-29 13:21:12 +0530

    Expanded SideNav

--- app/src/components/SideNav.js
+++ app/src/components/SideNav.js
@@ -1,17 +1,26 @@
 import React from "react";
 import "rsuite/dist/styles/rsuite-default.css";
-import { Dropdown, Icon, Nav, Sidenav } from "rsuite";
+import { Dropdown, Icon, Nav, Sidenav, Toggle } from "rsuite";
 import { useState } from "react";
 
 const SideNav = () => {
   const [activeKey, setActiveKey] = useState("1");
+  const [expanded, setExpanded] = useState(true);
+
   const updateActiveKey = (eventKey) => {
     console.log("hello");
     setActiveKey(eventKey);
   };
+
+  const handleToggle = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <div className="sidenav" style={{ width: 250 }}>
+      <Toggle onChange={handleToggle} checked={expanded} />
       <Sidenav
+        expanded={expanded}
         defaultOpenKeys={["2"]}
         activeKey={activeKey}
         onSelect={updateActiveKey}
