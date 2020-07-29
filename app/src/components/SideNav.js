e302f0ee219fc3a3762b1a41ff6c515a043425d6
zaryanz <aryanx404@gmail.com>
2020-07-29 12:22:33 +0530

    sidenav icons

--- app/src/components/SideNav.js
+++ app/src/components/SideNav.js
@@ -5,20 +5,20 @@ import { Dropdown, Icon, Nav, Sidenav } from "rsuite";
 const SideNav = () => {
   return (
     <div className="sidenav" style={{ width: 250 }}>
-      <Sidenav defaultOpenKeys={["3", "4"]} activeKey="1">
+      <Sidenav defaultOpenKeys={["2"]} activeKey="1">
         <Sidenav.Body>
           <Nav>
-            <Nav.Item eventKey="1" icon={<Icon icon="dashboard" />}>
+            <Nav.Item eventKey="1" icon={<Icon icon="th-list" />}>
               Todos
             </Nav.Item>
-            <Dropdown eventKey="3" title="Tags" icon={<Icon icon="magic" />}>
-              <Dropdown.Item eventKey="3-1">tag1</Dropdown.Item>
-              <Dropdown.Item eventKey="3-2">tag2</Dropdown.Item>
+            <Dropdown eventKey="2" title="Tags" icon={<Icon icon="tags" />}>
+              <Dropdown.Item eventKey="2-1">tag1</Dropdown.Item>
+              <Dropdown.Item eventKey="2-2">tag2</Dropdown.Item>
             </Dropdown>
-            <Nav.Item eventKey="4" icon={<Icon icon="dashboard" />}>
+            <Nav.Item eventKey="3" icon={<Icon icon="check" />}>
               Completed
             </Nav.Item>
-            <Nav.Item eventKey="5" icon={<Icon icon="dashboard" />}>
+            <Nav.Item eventKey="4" icon={<Icon icon="trash" />}>
               Trash
             </Nav.Item>
           </Nav>
