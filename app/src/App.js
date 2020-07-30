f8c8b319aa8c84f8fee9c054dd1bdfc0777be51b
zaryanz <aryanx404@gmail.com>
2020-07-30 10:08:51 +0530

    Overlay basic css

--- app/src/App.js
+++ app/src/App.js
@@ -6,17 +6,11 @@ function App() {
     <div style={{ display: "flex" }}>
       <SideNav />
 
-      <p>
+      <p style={{ marginLeft: "5em" }}>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus
         officiis velit repellat atque, alias a dolorum magnam cum pariatur? Ut
         quasi id placeat explicabo optio tempora esse deserunt, ipsum
         dignissimos.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi alias
-        repudiandae totam iste suscipit consequuntur esse voluptatum veniam
-        illum maxime quidem a architecto at accusantium similique, neque
-        voluptate quam illo?
       </p>
     </div>
   );
