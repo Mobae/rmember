5be80143e84242bb82a14262a1b7f3c6e84d6847
tars <ajaysharma2ku@gmail.com>
2020-07-31 12:51:53 +0530

    Final fix

--- app/src/App.js
+++ app/src/App.js
@@ -5,12 +5,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Todos from "./components/routes/Todos";
 import Completed from "./components/routes/Completed";
 import Trash from "./components/routes/Trash";
-<<<<<<< HEAD
-import DemoModal from '/components/'
-=======
 import LogIn from "./components/pages/LogIn";
 import SignUp from "./components/pages/SignUp";
->>>>>>> 01d32ee23b53cf9c165f964f31e0bdda001811ac
+import DemoModal from '/components/'
 
 function App() {
   return (
