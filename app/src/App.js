3624930a43a6759fb58ed3726590b95a86ac8687
zaryanz <aryanx404@gmail.com>
2020-07-31 13:18:38 +0530

    login signup router

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,24 @@
 import React from "react";
 import SideNav from "./components/layouts/SideNav";
-// import TopNav from "./components/layouts/TopNav";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Todos from "./components/routes/Todos";
 import Completed from "./components/routes/Completed";
 import Trash from "./components/routes/Trash";
 import LogIn from "./components/pages/LogIn";
 import SignUp from "./components/pages/SignUp";
-import DemoModal from '/components/'
 
 function App() {
   return (
     <Router>
       <div style={{ display: "flex" }}>
-        {/* <TopNav /> */}
         <SideNav />
-        <DemoModal />
       </div>
       <Switch>
         <Route exact path="/todos" component={Todos} />
         <Route exact path="/completed" component={Completed} />
         <Route exact path="/trash" component={Trash} />
+        <Route exact path="/login" component={LogIn} />
+        <Route exact path="/signup" component={SignUp} />
       </Switch>
     </Router>
   );
