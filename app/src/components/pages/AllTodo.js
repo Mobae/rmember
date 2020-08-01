c788bc3bf4bf57ddcdc0ea049572a612be97b70c
sageOMEGA <abhishekdhakad61@gmail.com>
2020-08-01 12:39:36 +0530

    Todo Card Basic

--- app/src/components/pages/AllTodo.js
+++ app/src/components/pages/AllTodo.js
@@ -1,59 +1,21 @@
 import React from "react";
+import "./AllTodo.css";
 import { Button } from "rsuite";
 
 const TodoCard = (serial) => {
   return (
-    <div
-      style={{
-        position: "relative",
-        width: "250px",
-        height: "360px",
-        background: "#fff",
-        margin: "0.5em",
-      }}
-    >
-      <div
-        style={{
-          position: "relative",
-          width: "100%",
-          height: "100%",
-          background: "#000",
-          clipPath: "circle(120px at center 0)",
-          textAlign: "center",
-        }}
-      >
-        {serial > 9 ? (
-          <h2 style={{ color: "#fff", fontSize: "3.5em", padding: "30px 0" }}>
-            {serial}
-          </h2>
-        ) : (
-          <h2 style={{ color: "#fff", fontSize: "3.5em", padding: "30px 0" }}>
-            0{serial}
-          </h2>
-        )}
+    <div className="card">
+      <div className="card-header">
+        {serial > 9 ? <h2>{serial}</h2> : <h2>0{serial}</h2>}
       </div>
-      <div
-        style={{
-          position: "absolute",
-          bottom: "10px",
-          padding: "20px",
-          textAlign: "center",
-        }}
-      >
-        <p style={{ color: "#666", fontSize: "12px" }}>
+      <div className="card-main-info">
+        <p style={{ color: "#666", fontSize: "10px" }}>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam
           labore tempore eaque in reprehenderit nostrum id, eos cum quidem fugit
           suscipit atque impedit adipisci consequatur nobis, consectetur animi
           nulla corrupti!
         </p>
-        <Button
-          style={{
-            backgroundColor: "#000",
-            color: "white",
-            marginTop: "20px",
-          }}
-          size="sm"
-        >
+        <Button size="xs" className="read-more-button">
           Read More
         </Button>
       </div>
@@ -63,18 +25,9 @@ const TodoCard = (serial) => {
 
 const AllTodo = () => {
   return (
-    <div
-      style={{
-        marginLeft: "30px",
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "center",
-        alignItems: "center",
-        minHeight: "100vh",
-        // backgroundAttachment: "fixed",
-      }}
-    >
-      {TodoCard(1)} {TodoCard(2)} {TodoCard(3)}{" "}
+    <div className="container">
+      {TodoCard(1)} {TodoCard(2)} {TodoCard(3)}
+      {TodoCard(4)}{" "}
     </div>
   );
 };
